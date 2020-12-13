58d440652b29a1e567b3649525614fa5f50872c1
Steve <stevebarakat@gmail.com>
2020-12-12 20:28:14 -0500

    jittery

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import { motion, useMotionValue, useAnimation, AnimatePresence } from "framer-motion";
+import { useAnimation, AnimatePresence } from "framer-motion";
 import { ListItem, ListItemContainer, EndCap, TaskText, BtnLink } from '../../styles/style';
 import { MdDragHandle } from 'react-icons/md';
 import { VscChromeClose } from 'react-icons/vsc';
@@ -17,15 +17,19 @@ const onDragEnd = (result, taskList, updateTaskList) => {
 
 const TaskList = ({ taskList, deleteTask, updateTaskList, updateTask, handleSetTaskList }) => {
   const controls = useAnimation();
-  
+
+  async function handleDrag(event, info, index) {
+    // if (info.offset.x < -100 || info.velocity.x < -500) {
+    //   await controls.start({ y: 0, transition: { duration: 0.5 } });
+    // }
+  }
+
   async function handleDragEnd(event, info, index) {
-    console.log(info.velocity.y)
-    console.log(info.offset.x)
     if (info.offset.x < -100 || info.velocity.x < -500) {
-      await controls.start({ x: 0, opacity: 1, transition: {duration: 1}, color: "red"});
-      deleteTask(index)
+      await controls.start({ y: 0, transition: { duration: 0.5 } });
+      await deleteTask(index);
     } else {
-      controls.start({ x: 0, opacity: 1, transition: "duration: 2.5" , color: "inherit"});
+      controls.start({ x: 0, opacity: 1, transition: { duration: 0.5 }, color: "red" });
     }
   }
 
@@ -47,54 +51,55 @@ const TaskList = ({ taskList, deleteTask, updateTaskList, updateTask, handleSetT
               >
                 {taskList.length === 0 ?
                   <p>You don't have any tasks.</p> :
-                  <div>
+                  <>
                     {taskList?.map((task, index) => {
                       return (
-                        <motion.div
-                          key={task.id} 
-                          drag="x"
-                          dragDirectionLock
-                          onDragEnd={(event, info) => handleDragEnd(event, info, index)}
-                          animate={controls}
-                          style={{background: 'hsla(216, 18%, 21%, 0.5)'}}
-                        >
-                          <Draggable draggableId={task.id} index={index}>
-                            {(provided, snapshot) => {
-                              return (
-                                <div
-                                  ref={provided.innerRef}
-                                  {...provided.draggableProps}
-                                  style={{
-                                    userSelect: 'none',
-                                    cursor: "grabbing",
-                                    opacity: snapshot.isDragging ? '0.5' : 1,
-                                    ...provided.draggableProps.style
-                                  }}
+                        <Draggable key={task.id} draggableId={task.id} index={index}>
+                          {(provided, snapshot) => {
+                            return (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                style={{
+                                  userSelect: 'none',
+                                  cursor: "grabbing",
+                                  opacity: snapshot.isDragging ? '0.5' : 1,
+                                  ...provided.draggableProps.style
+                                }}
+                              >
+                                <ListItemContainer
+                                  drag="x"
+                                  // dragConstraints={{ top: 0, bottom: 0 }}
+                                  dragDirectionLock
+                                  // layout
+                                  onDrag={(event, info) => handleDrag(event, info, index)}
+                                  onDragEnd={(event, info) => handleDragEnd(event, info, index)}
+                                  animate={controls}
+                                  exit={{ y: 0 }}
+                                  style={{ background: 'hsla(216, 18%, 21%, 0.5)' }}
                                 >
-                                  <ListItemContainer>
-                                    <BtnLink task onClick={() => deleteTask(index)}><VscChromeClose /></BtnLink>                                  <ListItem>
-                                      <TaskText
-                                        contentEditable
-                                        suppressContentEditableWarning
-                                        onBlur={e => {
-                                          updateTask(e, task.id);
-                                        }}
-                                      >
-                                        {task.title}
-                                      </TaskText>
-                                    </ListItem>
-                                    <EndCap {...provided.dragHandleProps}>
-                                      <MdDragHandle style={{ margin: "auto" }} />
-                                    </EndCap>
-                                  </ListItemContainer>
-                                </div>
-                              );
-                            }}
-                          </Draggable>
-                        </motion.div>
+                                  <BtnLink task onClick={() => deleteTask(index)}><VscChromeClose /></BtnLink>                                  <ListItem>
+                                    <TaskText
+                                      contentEditable
+                                      suppressContentEditableWarning
+                                      onBlur={e => {
+                                        updateTask(e, task.id);
+                                      }}
+                                    >
+                                      {task.title}
+                                    </TaskText>
+                                  </ListItem>
+                                  <EndCap {...provided.dragHandleProps}>
+                                    <MdDragHandle style={{ margin: "auto" }} />
+                                  </EndCap>
+                                </ListItemContainer>
+                              </div>
+                            );
+                          }}
+                        </Draggable>
                       );
                     })}
-                  </div>}
+                  </>}
                 {provided.placeholder}
               </div>
             );
